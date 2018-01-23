23c3db2928c73f295fb0fb679afb69ce756186e4
MW <m.wroblewski3d@gmail.com>
2018-01-23 17:43:37 +0100

    dfs

--- js_scripts/script.js
+++ js_scripts/script.js
@@ -1,1 +1,28 @@
-var x;
+
+function drawLine(){
+
+
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+
+
+      context.beginPath();
+      context.moveTo(100, 20);
+
+      // line 1
+      context.lineTo(200, 160);
+
+      // quadratic curve
+      context.quadraticCurveTo(230, 200, 250, 120);
+
+      // bezier curve
+      context.bezierCurveTo(290, -40, 300, 200, 400, 150);
+
+      // line 2
+      context.lineTo(500, 90);
+
+      context.lineWidth = 5;
+      context.strokeStyle = 'blue';
+      context.stroke();
+
+}
